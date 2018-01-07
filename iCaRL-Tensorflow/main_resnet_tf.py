b90f53f389a59b0bfbf28822f98f9632b1c1e49c
Yu Hao <jinyu121@126.com>
2018-01-07 14:08:19 +0800

    Fix bytes[] and string missmatch error

--- iCaRL-Tensorflow/main_resnet_tf.py
+++ iCaRL-Tensorflow/main_resnet_tf.py
@@ -188,6 +188,7 @@ for itera in range(nb_groups):
     
     # Load the training samples of the current batch of classes in the feature space to apply the herding algorithm
     Dtot,processed_files,label_dico = utils_icarl.load_class_in_feature_space(files_from_cl, batch_size, scores, label_batch, loss_class, file_string_batch, op_feature_map, sess)
+    processed_files = np.array([x.decode() for x in processed_files])
     
     # Herding procedure : ranking of the potential exemplars
     print('Exemplars selection starting ...')
@@ -221,6 +222,7 @@ for itera in range(nb_groups):
           void2   = sess.run(inits)
           
           Dtot,processed_files,label_dico = utils_icarl.load_class_in_feature_space(files_from_cl, batch_size, scores, label_batch, loss_class, file_string_batch, op_feature_map, sess)
+          processed_files = np.array([x.decode() for x in processed_files])
           
           for iter_dico in range(nb_cl):
               ind_cl     = np.where(label_dico == order[iter_dico+iteration2*nb_cl])[0]
